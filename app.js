f6b6c1045fdaee17d75e61a6f2d0d467558718f7
diegoejp <d.jacobpulgar@gmail.com>
2021-06-02 16:52:21 -0400

    falta aun...

--- app.js
+++ app.js
@@ -1,23 +1,36 @@
-let formu = document.querySelector("#formulario");
+//variables globales
+let nombrevalido = false;
 
+
+let formu = document.querySelector("#formulario");
 function prevenir (e){
     e.preventDefault();
 }
 validarCard();
 validarCVC();
 
+//Funcion para mandar formulario s iesta validado
+let enviar = document.querySelector("#send");
+enviar.addEventListener("click",(e)=>{
+    validarNombre();
+    if(nombrevalido == false){
+        e.preventDefault();
+    }
+    console.log(nombrevalido);
+});
 
 
 //regex
 let regexCard = /^[0-9]{13,16}$/;
 let regexCVC = /^[\d]{3}$/;
+let regNom = /[a-zA-Z]{3,}/;
 
 
 function validarCard(){
     let card = document.querySelector("#card");
-    card.addEventListener("input", (e)=>{
+     card.addEventListener("input", (e)=>{
         let textCard = e.target.value.trim();
-        console.log(textCard);
+        // console.log(textCard);
         if(textCard == "" || !regexCard.test(textCard)){
             card.classList.add("is-invalid");
             card.classList.remove("is-valid");
@@ -28,7 +41,7 @@ function validarCard(){
             card.classList.add("is-valid");
             formu.removeEventListener("submit",prevenir);
         }
-    });
+     });
 }
 
 function validarCVC (){
@@ -36,9 +49,22 @@ function validarCVC (){
     cvc.addEventListener("input",(e)=>{
         let textCVC = e.target.value.trim();
         if(textCVC == "" || !regexCVC.test(textCVC)){
-            console.log("NO");
+            cvc.classList.add("is-invalid");
         }else{
-            console.log("SI");
+            cvc.classList.remove("is-invalid");
+            cvc.classList.add("is-valid");
         }
     });
+}
+
+function validarNombre(){
+    let name = document.querySelector("#name");
+    let textName = name.value.trim();
+    if(textName == "" || !regNom.test(textName)){
+        name.classList.add("is-invalid");
+        nombrevalido = false;
+    }else{
+        name.classList.add("is-valid");
+        nombrevalido = true;
+    }
 }
